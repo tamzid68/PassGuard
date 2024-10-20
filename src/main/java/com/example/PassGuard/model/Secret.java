e47b549da4b38a1fe2127aaa66ce286070ba7919
ASM Tamzid <atkthegreat9999@gmail.com>
2024-10-21 01:12:17 +0600

    added Model and Directory Structure

---
+++ src/main/java/com/example/PassGuard/model/Secret.java
@@ -0,0 +1,18 @@
+package com.example.PassGuard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+public class Secret {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String serviceName;
+    private String encryptedPassword;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/example/PassGuard/model/User.java
@@ -0,0 +1,19 @@
+package com.example.PassGuard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Secret> secrets;
+}
