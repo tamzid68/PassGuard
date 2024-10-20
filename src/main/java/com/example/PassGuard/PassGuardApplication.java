755453fb191b2d45ce4713f4cc2d99ce9f7f1097
ASM Tamzid <atkthegreat9999@gmail.com>
2024-10-20 23:11:24 +0600

    new project create

---
+++ src/main/java/com/example/PassGuard/PassGuardApplication.java
@@ -0,0 +1,15 @@
+package com.example.PassGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassGuardApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PassGuardApplication.class, args);
+		System.out.println("OKAY");
+	}
+
+}
---
+++ src/test/java/com/example/PassGuard/PassGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PassGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
